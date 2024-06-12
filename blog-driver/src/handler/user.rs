5016cff57915a81129474f0370d7aad5e6314bb3
AKUU <57586401+Akuu29@users.noreply.github.com>
2024-06-13 00:46:24 +0900

    Fix: Fixed Extensions

--- blog-driver/src/handler/user.rs
+++ blog-driver/src/handler/user.rs
@@ -7,11 +7,12 @@ use axum::{
 use blog_app::{
     model::user::{SigninUser, SignupUser},
     repository::user::UserRepository,
+    usecase::user::UserUseCase,
 };
 use std::sync::Arc;
 
 pub async fn signup<T: UserRepository>(
-    Extension(user_repository): Extension<Arc<T>>,
+    Extension(user_repository): Extension<Arc<UserUseCase<T>>>,
     ValidatedJson(payload): ValidatedJson<SignupUser>,
 ) -> Result<impl IntoResponse, StatusCode> {
     let user = user_repository
@@ -23,7 +24,7 @@ pub async fn signup<T: UserRepository>(
 }
 
 pub async fn signin<T: UserRepository>(
-    Extension(user_repository): Extension<Arc<T>>,
+    Extension(user_repository): Extension<Arc<UserUseCase<T>>>,
     ValidatedJson(payload): ValidatedJson<SigninUser>,
 ) -> Result<impl IntoResponse, StatusCode> {
     let user = user_repository
