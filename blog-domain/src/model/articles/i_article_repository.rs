54f4cf76897cf68df4954fc6f2230633838ff6e5
AKUU <57586401+Akuu29@users.noreply.github.com>
2024-08-13 15:40:52 +0900

    Modify: Renamed trait "ArticleRepository" to "IArticleRepository"

--- blog-domain/src/model/articles/i_article_repository.rs
+++ blog-domain/src/model/articles/i_article_repository.rs
@@ -2,8 +2,8 @@ use crate::model::articles::article::{Article, NewArticle, UpdateArticle};
 use async_trait::async_trait;
 
 #[async_trait]
-pub trait ArticleRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
-    async fn create(&self, payload: NewArticle) -> anyhow::Result<Article>;
+pub trait IArticleRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
+    async fn create(&self, user_id: i32, payload: NewArticle) -> anyhow::Result<Article>;
     async fn find(&self, id: i32) -> anyhow::Result<Article>;
     async fn all(&self) -> anyhow::Result<Vec<Article>>;
     async fn update(&self, id: i32, payload: UpdateArticle) -> anyhow::Result<Article>;
