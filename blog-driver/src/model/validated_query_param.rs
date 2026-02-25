7ba3c9d283ecaab5f0f1f38b940a6560cc8f8718
AKUU <57586401+Akuu29@users.noreply.github.com>
2026-02-25 16:39:02 +0900

    Fix: Use serde_qs Config to explicitly control query string deserialization depth

--- blog-driver/src/model/validated_query_param.rs
+++ blog-driver/src/model/validated_query_param.rs
@@ -24,7 +24,8 @@ where
     #[tracing::instrument(name = "validated_query_param")]
     async fn from_request(req: Request, _: &B) -> Result<Self, Self::Rejection> {
         let query_string = req.uri().query().unwrap_or_default();
-        let val = serde_qs::from_str::<T>(query_string).map_err(|e| {
+        let qs_config = serde_qs::Config::new(5, false);
+        let val = qs_config.deserialize_str::<T>(query_string).map_err(|e| {
             tracing::error!(error.kind="Unexpected", error.message=%e.to_string());
 
             let res_body = ErrorResponse::new(
